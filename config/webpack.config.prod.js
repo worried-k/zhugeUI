8447a6921fc6891d6f30acad6c0970921cf2b80c
董永强 <yqdong@37degree.com>
2017-09-06 17:18:55 +0800

    添加打包命令

---
+++ config/webpack.config.prod.js
@@ -0,0 +1,14 @@
+/**
+ * Created by yqdong on 2017/9/6.
+ * qq: 1013501639
+ * @author yqdong
+ *
+ */
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.config.base')
+
+const config = merge(baseConfig, {
+
+})
+
+module.exports = config
