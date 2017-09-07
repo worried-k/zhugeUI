f859f54f4b6effc045cf974083d7ae7775634e55
董永强 <yqdong@37degree.com>
2017-09-07 15:42:41 +0800

    button组件完成

---
+++ src/components/button/button.js
@@ -0,0 +1,143 @@
+/**
+ * Created by yqdong on 2017/9/6.
+ * qq: 1013501639
+ * @author yqdong
+ *
+ */
+import util from '../../utils/util'
+
+import '../../assets/styles/reset.sass'
+import './styles/button.sass'
+
+import template from './template/button.html'
+function Button (container, option) {
+  let options = util.mergeObject({
+    theme: 'normal', // normal,border
+    type: 'normal', // normal, primary, danger, secondary, success
+    size: 'normal', // normal small large
+    disabled: false,
+
+    content: '',
+
+    icon: {
+      show: false,
+      iconClass: ''
+    },
+
+    onClick: null
+  }, option)
+
+  for (let prop in options) {
+    this['_' + prop] = options[prop]
+  }
+  this._dom = {
+    container: container,
+    icon: null,
+    content: null
+  }
+
+  this._init()
+}
+
+Button.prototype = {
+  constructor: Button,
+  /**
+   *
+   * @private
+   */
+  _init: function () {
+    this._render()
+    this._initEventBind()
+  },
+  /**
+   *
+   * @private
+   */
+  _render: function () {
+    this._resetStyle()
+    this._dom.container.append(util.strReplace(template, {
+      iconClass: this._icon.iconClass,
+      displayIcon: this._icon.show ? 'inline-block' : 'none',
+      content: this._content
+    }))
+
+    this._dom.content = this._dom.container.find('span')
+    this._dom.icon = this._dom.container.find('i')
+  },
+  /**
+   *
+   * @private
+   */
+  _resetStyle: function () {
+    this._dom.container.attr('class', '')
+
+    let mainClass = this._theme + '-' + this._type
+
+    this._dom.container.addClass('zg-button')
+      .addClass((this._icon.show && !this._content) ? 'only-icon' : '')
+      .addClass(this._theme === this._type ? 'normal' : mainClass)
+      .addClass(this._size === 'normal' ? 'normal-size' : this._size)
+
+    if (this._disabled) {
+      this._dom.container.addClass(this._theme === 'normal' ? 'normal-disable' : 'border-disable')
+    }
+  },
+  /**
+   *
+   * @private
+   */
+  _initEventBind: function () {
+    this._dom.container.bind('click', this, this.__onClick)
+  },
+  /**
+   *
+   * @private
+   */
+  __onClick: function (event) {
+    let context = event.data
+    const callback = context._onClick
+    if (util.isFunction(callback)) {
+      callback.call(context)
+    }
+  },
+  /**
+   *
+   * @param flag
+   * @returns {Button}
+   */
+  disable: function (flag) {
+    this._disabled = flag
+    this._resetStyle()
+    return this
+  },
+  /**
+   *
+   * @param type
+   * @returns {Button}
+   */
+  setType: function (type) {
+    this._type = type
+    this._resetStyle()
+    return this
+  },
+  /**
+   *
+   * @param content
+   * @returns {Button}
+   */
+  setContent: function (content) {
+    this._content = content
+    this._dom.content.text(content)
+    this._resetStyle()
+    return this
+  },
+  /**
+   *
+   */
+  destroy: function () {
+    this._dom.container.unbind('click')
+    this._dom.container.remove()
+  }
+}
+
+export default Button
--- src/components/demo/demo.js
+++ src/components/demo/demo.js
@@ -7,10 +7,12 @@
 
 import template from './template/index.html'
 import './styles/main.sass'
+import util from '../../utils/util'
 
 function Demo () {
   const name = 'demo'
   console.log(`this is ${name}!!`)
+  console.log(util)
   document.body.innerHTML = template
 }
 
